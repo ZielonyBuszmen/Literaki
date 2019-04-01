37a2717836a58ffaa20054e0392d246fe8211c5e
Patrycja <madydapatrycja97@o2.pl>
2019-04-01 13:20:22 +0200

    Dodanie pozostałych plików :D

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends React.Component {
+  render () {
+    return (
+      <div className="about">
+        <p>Super Duper Example App!</p>
+      </div>
+    );
+  }
+}
+
+export default About;
