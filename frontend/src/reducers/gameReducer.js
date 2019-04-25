9ac1ae3ad3beb3e82333ceb6d2b4fad333f94983
Patrycja <madydapatrycja97@o2.pl>
2019-04-25 16:12:23 +0200

    Naprawa głupiego/ciekawego błędu

--- frontend/src/reducers/gameReducer.js
+++ frontend/src/reducers/gameReducer.js
@@ -1,6 +1,7 @@
 //type": "BE_GAME_STATE", "password": "________ _____ _ ____", "category": "biedota"}
 
-export const websocketReducer = (state = [], action) => {
+export const gameReducer = (state = [], action) => {
+    console.log('asaasasdsa',action)
     switch (action.type) {
         case 'BE_GAME_STATE':
           return {...state, catchword: action.catchword }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {lobbyReducer} from "./lobbyReducer";
 import {websocketReducer} from "./websocketRedcer";
+import {gameReducer} from "./gameReducer";
 
 export default combineReducers({
     lobby: lobbyReducer,
     websocket: websocketReducer,
+    game: gameReducer,
 });
