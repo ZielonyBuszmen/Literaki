533f4aa7f99db2db648ecb37f6a41e5d8fadeca3
Patrycja <madydapatrycja97@o2.pl>
2019-04-29 21:23:45 +0200

    Zwiększenie bazy haseł.

--- backend/Gameplay/catchword_list.py
+++ backend/Gameplay/catchword_list.py
@@ -3,15 +3,116 @@ catchword = 'catchword'
 
 CATCHWORDS = [
     {
-        category: 'sypialnia',
-        catchword: 'jak sobie pościelisz, tak się wyśpisz',
+        category: 'Motywacja',
+        catchword: 'zrób dziś, co masz zrobić jutro.',
     },
     {
-        category: 'biedota',
-        catchword: 'biednemu wiatr w oczy',
+        category: 'Motywacja',
+        catchword: 'dla chcącego nic trudnego.',
     },
     {
-        category: 'zwierzeta',
-        catchword: 'jakby kózka nie skakała, to by nogi nie złamała',
+        category: 'Motywacja',
+        catchword: 'co cię nie zabije, to cię wzmocni.',
+    },
+    {
+        category: 'Motywacja',
+        catchword: 'każdy jest kowalem swego losu.',
+    },
+    {
+        category: 'Pogoda',
+        catchword: 'jedna jaskółka wiosny nie czyni.',
+    },
+    {
+        category: 'Pogoda',
+        catchword: 'w marcu jak w garncu.',
+    },
+    {
+        category: 'Pogoda',
+        catchword: 'kwiecień plecień, bo przeplata trochę zimy, trochę lata.',
+    },
+    {
+        category: 'Pogoda',
+        catchword: 'idzie luty, podkuj buty.',
+    },
+    {
+        category: 'Pogoda',
+        catchword: 'z dużej chmury mały deszcz.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'w zdrowym ciele zdrowy duch.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'co za dużo, to niezdrowo.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'zdrowego leczyć nie trzeba.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'czym się stułeś, tym się lecz.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'zdrowy jak koń.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'śmiech to zdrowie.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'stara miłość nie rdzewieje.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'miłość cierpliwa jest, łaskawa jest.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'raz tylko w życiu kochamy i raz tylko umieramy.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'kochać, a nie być kochanym, najnieszczęśliwszym jest stanem.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'kochają się jak dwa gołąbki.',
+    },
+    {
+        category: 'Biesiada',
+        catchword: 'ten żyje, kto pije!',
+    },
+    {
+        category: 'Biesiada',
+        catchword: 'wódka rozwiązuje języki nawet tym, którzy nabrali wody w usta.',
+    },
+
+    {
+        category: 'Biesiada',
+        catchword: 'od wódki rozum krótki.',
+    },
+    {
+        category: 'Biesiada',
+        catchword: 'od piwa głowa się kiwa.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'lepiej późno niż wcale.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'szczęśliwi czasu nie liczą.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'komu w drogę, temu czas.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'choćbyś uciekał przez góry, las – śmierć do ciebie zdąży na czas.',
     },
 ]
