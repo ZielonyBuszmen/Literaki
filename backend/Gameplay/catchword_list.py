0c38dcbdd63d52d9338c3daf48a0af7115ee668a
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2019-05-01 21:59:36 +0200

    poprawka

--- backend/Gameplay/catchword_list.py
+++ backend/Gameplay/catchword_list.py
@@ -43,6 +43,79 @@ CATCHWORDS = [
         catchword: 'w zdrowym ciele zdrowy duch.',
     },
     {
+        category: 'Zdrowie',
+        catchword: 'co za dużo, to niezdrowo.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'zdrowego leczyć nie trzeba.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'czym się stułeś, tym się lecz.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'zdrowy jak koń.',
+    },
+    {
+        category: 'Zdrowie',
+        catchword: 'śmiech to zdrowie.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'stara miłość nie rdzewieje.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'miłość cierpliwa jest, łaskawa jest.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'raz tylko w życiu kochamy i raz tylko umieramy.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'kochać, a nie być kochanym, najnieszczęśliwszym jest stanem.',
+    },
+    {
+        category: 'Miłość',
+        catchword: 'kochają się jak dwa gołąbki.',
+    },
+    {
+        category: 'Biesiada',
+        catchword: 'ten żyje, kto pije!',
+    },
+    {
+        category: 'Biesiada',
+        catchword: 'wódka rozwiązuje języki nawet tym, którzy nabrali wody w usta.',
+    },
+
+    {
+        category: 'Biesiada',
+        catchword: 'od wódki rozum krótki.',
+    },
+    {
+        category: 'Biesiada',
+        catchword: 'od piwa głowa się kiwa.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'lepiej późno niż wcale.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'szczęśliwi czasu nie liczą.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'komu w drogę, temu czas.',
+    },
+    {
+        category: 'Czas',
+        catchword: 'choćbyś uciekał przez góry, las – śmierć do ciebie zdąży na czas.',
+    },
+    {
         category: 'Studia',
         catchword: 'raz sierpem raz młotem studencką chołotę!',
     },
