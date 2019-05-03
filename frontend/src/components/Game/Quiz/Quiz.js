0f28938c5c9e5d1b3a5891f41ca151e2718a0148
Patrycja Madyda <madydapatrycja97@o2.pl>
2019-05-03 18:55:30 +0200

    Formatowanie kodu

--- frontend/src/components/Game/Quiz/Quiz.js
+++ frontend/src/components/Game/Quiz/Quiz.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {Row, Col} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import "./Quiz.css";
 
 class Quiz extends React.Component {
-        render() {
-        let infoTurn = this.props.yourTurn ? 'Twoja tura!' : '';
-        if(this.props.result!== "") infoTurn = '';
-        return (
-            <div className="Quiz h-100">
-                <Row className='align-items-center h-75'>
-                    <Col className='catchword col-xl-6 col-lg-10 col-sm-12 mx-auto'>{this.props.catchword}</Col>
-                </Row>
-                <Row>
-                    <Col className="result">{this.props.result}</Col>
-                </Row>
-                <Row>
-                    <Col className="infoTurn">{infoTurn}</Col>
-                </Row>
-            </div>
-        );
-    }
+  render() {
+    let infoTurn = this.props.yourTurn ? 'Twoja tura!' : '';
+    if (this.props.result !== "") infoTurn = '';
+    return (
+      <div className="Quiz h-100">
+        <Row className='align-items-center h-75'>
+          <Col className='catchword col-xl-6 col-lg-10 col-sm-12 mx-auto'>{this.props.catchword}</Col>
+        </Row>
+        <Row>
+          <Col className="result">{this.props.result}</Col>
+        </Row>
+        <Row>
+          <Col className="infoTurn">{infoTurn}</Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Quiz;
--- frontend/src/reducers/chatReducer.js
+++ frontend/src/reducers/chatReducer.js
@@ -1,28 +1,28 @@
 import {
-    BE_CHAT_MESSAGE,
-    BE_PLAYER_DISCONNECTED_FROM_GAMEPLAY
+  BE_CHAT_MESSAGE,
+  BE_PLAYER_DISCONNECTED_FROM_GAMEPLAY
 } from "../actions";
 
 export const GAME_END = 'GAME_END';
 
 export const chatReducer = (state = [], action) => {
-    switch (action.type) {
-        case BE_CHAT_MESSAGE:
-            return {
-                ...state,
-                messages: [...state.messages, {
-                    time: action.time,
-                    isCurrentPlayer: action.isCurrentPlayer,
-                    message: action.message
-                }],
-            };
-            case BE_PLAYER_DISCONNECTED_FROM_GAMEPLAY:
-            return {
-                messages: [...state.messages, {
-                    message: GAME_END
-                }],
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case BE_CHAT_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, {
+          time: action.time,
+          isCurrentPlayer: action.isCurrentPlayer,
+          message: action.message
+        }],
+      };
+    case BE_PLAYER_DISCONNECTED_FROM_GAMEPLAY:
+      return {
+        messages: [...state.messages, {
+          message: GAME_END
+        }],
+      };
+    default:
+      return state;
+  }
 };
